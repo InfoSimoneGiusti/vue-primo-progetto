0aa4d96c4aabc4d5d824ebb88e504d1032d92688
Simone Giusti <info@simonegiusti.it>
2023-03-24 13:18:09 +0100

    LC concluso

---
+++ src/components/ListaSpesa.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul>
+        <li>Pane</li>
+        <li>Pasta</li>
+        <li>Uova</li>
+    </ul>
+
+    <img src="../../public/vite.svg" />
+    <img src="/vite.svg" />
+
+</template>
+
+
+<script>
+
+</script>
+
+
+<style>
+
+</style>
---
+++ src/components/Titolo.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Ciao Classe 87!</h1>
+</template>
+
+
+<script>
+
+</script>
+
+<style scoped > 
+    h1 {
+        color: red;
+    }
+</style>
